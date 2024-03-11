5c5133da2271be84c9018c95765a0a0ccce72021
Arnaud <arnaduga@gmail.com>
2024-03-11 19:40:38 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Really Simple Timer
+
+Daily stand-up meetings... Every day, a 15-minutes meeting is scheduled to let every team member explained what he/she has done yesterday, and what he/she will do today.
+
+Nice!
+
+... but... 15 minutes for 7 people mean not more than 2 minutes by speaker (considering it start on time)
+
+## Goal of this "application
+
+This is because we _never_ respected the 15 minutes timeslot this timer could help: it will display a very basic timer, to be share with co-workers during the daily, to let them know how much time it remains.
+
+## How to use?
+
+This is a static website: you just need to open the `index.html` in a web browser.
+
+## How to set time?
+
+In your web browser URL just add a query string `duration`!
+
+For instance:
+
+| Duration   | URL                         |
+| ---------- | --------------------------- |
+| 4 minutes  | `index.html?duration=4:00`  |
+| 2 minutes  | `index.html?duration=2:00`  |
+| 45 minutes | `index.html?duration=45:00` |
+| 4 minutes  | `index.html?duration=4:00`  |
+
+
+That's it! :smile:
+
+
+## What else?
+
+For the moment, nothing!
+
+Some other features may come... but the goal is really to keep this as simple/basic as possible!
+
+
+# Licenses
+
+The background picture comes from [Benjamin Child](https://unsplash.com/fr/@bchild311?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash) on [Unsplash](https://unsplash.com/fr/photos/table-et-chaises-de-conference-ovales-en-bois-brun-a-linterieur-de-la-salle-de-conference-GWe0dlVD9e0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash).
+
+The notification sound comes is # Notification sound license Ominant by [Juril](https://pixabay.com/users/soundreality-31074404/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=163603) from [Pixabay](https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=163603).
+
+The font **Dosis** is licensed under the [Open Font License](https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL), from [Pablo Impallari](https://twitter.com/impallaritype).
---
+++ scripts/script.js
@@ -0,0 +1,105 @@
+let countdown;
+const timerDisplay = document.getElementById("timer");
+const startButton = document.getElementById("startButton");
+const pauseButton = document.getElementById("pauseButton");
+const resetButton = document.getElementById("resetButton");
+const timerContainer = document.getElementById("timer-container");
+const audio = new Audio("./sounds/round_ended.mp3");
+let isTimerRunning = false;
+
+function getDurationFromQueryString() {
+  const params = new URLSearchParams(window.location.search);
+  const duration = params.get("duration");
+  if (duration && /^(\d+):(\d+)$/.test(duration)) {
+    const [minutes, seconds] = duration
+      .split(":")
+      .map((num) => parseInt(num, 10));
+    return minutes * 60 + seconds;
+  }
+  return 2 * 60; // Default to 180 seconds (3 minutes) if no valid duration is provided
+}
+
+function timer(seconds) {
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    displayTimeLeft(secondsLeft);
+
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+
+      audio.play();
+      timerDisplay.classList.add("timeout");
+      timerDisplay.classList.add("pulsate");
+      startButton.classList.add("hidden");
+      pauseButton.classList.add("hidden");
+      resetButton.classList.remove("hidden");
+      return;
+    }
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  let display = "00:00"
+  if (seconds && seconds >= 0) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    display = `${minutes}:${
+      remainderSeconds < 10 ? "0" : ""
+    }${remainderSeconds}`;
+  } 
+
+  timerDisplay.textContent = display;
+}
+
+// Initialize timer display based on query string
+const initialSeconds = getDurationFromQueryString();
+timerContainer.classList.remove("hidden");
+displayTimeLeft(initialSeconds);
+timerContainer.classList.remove("hidden");
+
+startButton.addEventListener("click", () => {
+  if (!isTimerRunning) {
+    timer(initialSeconds);
+    isTimerRunning = true;
+    startButton.classList.add("hidden");
+    pauseButton.classList.remove("hidden");
+    resetButton.classList.remove("hidden");
+  }
+});
+
+pauseButton.addEventListener("click", () => {
+  if (isTimerRunning) {
+    clearInterval(countdown);
+    isTimerRunning = false;
+    pauseButton.textContent = "Resume";
+    timerDisplay.classList.add("paused");
+    timerDisplay.classList.add("pulsate");
+  } else {
+    const timeArray = timerDisplay.textContent.split(":");
+    const secondsRemaining =
+      parseInt(timeArray[0]) * 60 + parseInt(timeArray[1]);
+    timer(secondsRemaining);
+    isTimerRunning = true;
+    pauseButton.textContent = "Pause";
+    timerDisplay.classList.remove("paused");
+    timerDisplay.classList.remove("pulsate");
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  clearInterval(countdown);
+  isTimerRunning = false;
+  displayTimeLeft(getDurationFromQueryString());
+  timerDisplay.classList.remove("timeout");
+  timerDisplay.classList.remove("pulsate");
+  startButton.classList.remove("hidden");
+  resetButton.classList.add("hidden");
+  pauseButton.classList.add("hidden");
+});
